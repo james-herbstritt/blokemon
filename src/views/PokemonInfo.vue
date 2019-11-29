d26b92e07442e3f9a558a4b89ac4052b3b58dd34
James Herbstritt <james.herbstritt@gmail.com>
2019-11-29 00:13:17 -0500

    Start exposing pokemon info through template

--- src/views/PokemonInfo.vue
+++ src/views/PokemonInfo.vue
@@ -1,7 +1,41 @@
 <template>
   <div>
     <div v-if="$store.state.selectedPokemon" class="pokemon-info" id="pokemon-info">
-    {{ $store.state.selectedPokemon }}
+       <img v-bind:src="$store.state.selectedPokemon.sprites.front_default" alt="Image
+       not found">
+
+       <div v-if="$store.state.selectedPokemon.types.length === 1">
+         <h3> Type </h3>
+         <p>
+           {{ $store.state.selectedPokemon.types[0].type.name }}
+         </p>
+       </div>
+       <div v-if="$store.state.selectedPokemon.types.length === 2">
+         <h3> Types </h3>
+         <p>
+           {{ $store.state.selectedPokemon.types[0].type.name }} |
+            {{ $store.state.selectedPokemon.types[1].type.name }}
+         </p>
+       </div>
+
+       <h2> {{ $store.state.selectedPokemon.name }} </h2>
+       <h3> Abilities </h3>
+       <p v-for="ability in $store.state.selectedPokemon.abilities"
+          v-bind:key="ability.url">
+         {{ ability.ability.name }}
+       </p>
+
+       <h3> Moves </h3>
+       <p v-for="move in $store.state.selectedPokemon.moves"
+          v-bind:key="move.url">
+         {{ move.move.name }}
+       </p>
+
+       <h3> Base Stats </h3>
+       <p v-for="stat in $store.state.selectedPokemon.stats"
+          v-bind:key="stat.stat.url">
+               {{ stat.stat.name }}: {{stat.base_stat}}
+       </p>
     </div>
     <button @click="getPokemon">
         get Bulbasaur
@@ -19,7 +53,7 @@ export default class PokemonInfo extends Vue {
 
   getPokemon ():void {
     PokemonService.getInstance().getPokemonById('1')
-      .then(pokemon => { this.$store.commit('selectPokemon', JSON.stringify(pokemon)) })
+      .then(pokemon => { this.$store.commit('selectPokemon', pokemon) })
   };
 }
 </script>
