065669db43921771062f60e3c0aa829a4f14524b
James Herbstritt <james.herbstritt@gmail.com>
2019-12-14 19:27:33 -0500

    Add search input with no functionality

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,18 @@
 <template>
-  <h2> Welcome Home </h2>
+  <div>
+    <form autocomplete="on" id="searchForm">
+      <label for="pokemonSearch">Search for a Pokemon: </label>
+      <input type="search" id="pokemonSearch" maxlength="20" form="searchForm"
+             placeholder="Pokemon Name"/>
+    </form>
+  </div>
 </template>
 
 <script lang="ts">
 import { Component, Prop, Vue } from 'vue-property-decorator'
 
 @Component
-export default class Home extends Vue {
-}
+export default class Home extends Vue {}
 </script>
 
-<style scoped lang="less">
-
-</style>
+<style scoped lang="less"></style>
