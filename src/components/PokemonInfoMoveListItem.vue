2bf151d847decfc1a615570d0c2b48b8d06fff70
james-herbstritt <james.herbstritt@gmail.com>
2020-03-03 18:07:42 -0500

    Extract list item in Info to this componen

---
+++ src/components/PokemonInfoMoveListItem.vue
@@ -0,0 +1,24 @@
+<template>
+  <li v-bind:id="id" class="list-group-item">
+    {{ move.move.name }}
+  </li>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator'
+
+@Component({
+  computed: {
+    id (): string {
+      // TODO:: maybe use key here?
+      return 'move-list-item-' + this.move.move.name
+    }
+  }
+})
+export default class PokemonInfoMoveListItem extends Vue {
+  @Prop() move: object
+  @Prop() key: string
+}
+</script>
+
+<style scoped lang="less"></style>
