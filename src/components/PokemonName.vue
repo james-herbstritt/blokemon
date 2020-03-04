638fcf124a87a0215fd16acbbaa4b71ba13c1a30
james-herbstritt <james.herbstritt@gmail.com>
2020-03-03 22:17:17 -0500

    Start base stat table

---
+++ src/components/PokemonName.vue
@@ -0,0 +1,20 @@
+<template>
+  <h2 id="pokemon-name">
+    {{ name[0].toUpperCase() + name.slice(1) }}
+  </h2>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator'
+
+@Component
+export default class PokemonInfoMoveListItem extends Vue {
+  @Prop() name: string
+}
+</script>
+
+<style lang="less">
+h2 {
+  border: black;
+}
+</style>
--- src/views/PokemonInfo.vue
+++ src/views/PokemonInfo.vue
@@ -1,30 +1,28 @@
 <template>
-  <div>
+  <div class="container container-fluid">
     <div
       v-if="$store.state.selectedPokemon"
       class="pokemon-info"
       id="pokemon-info"
     >
-      <h2>
-        {{
-          $store.state.selectedPokemon.name[0].toUpperCase() +
-            $store.state.selectedPokemon.name.slice(1)
-        }}
-      </h2>
       <img
+        id="pokemon-image"
         v-bind:src="$store.state.selectedPokemon.sprites.front_default"
         alt="Image
        not found"
       />
+      <PokemonName
+        id="pokemon-name"
+        v-bind:name="$store.state.selectedPokemon.name"
+      />
 
+      <h3>Type</h3>
       <div v-if="$store.state.selectedPokemon.types.length === 1">
-        <h3>Type</h3>
         <p>
           {{ $store.state.selectedPokemon.types[0].type.name }}
         </p>
       </div>
       <div v-if="$store.state.selectedPokemon.types.length === 2">
-        <h3>Types</h3>
         <p>
           {{ $store.state.selectedPokemon.types[0].type.name }} |
           {{ $store.state.selectedPokemon.types[1].type.name }}
@@ -40,18 +38,23 @@
       </p>
 
       <h3>Base Stats</h3>
-      <p
-        v-for="stat in $store.state.selectedPokemon.stats"
-        v-bind:key="stat.stat.url"
-      >
-        {{ stat.stat.name }}: {{ stat.base_stat }}
-      </p>
+      <ul class="list-group">
+        <li
+          v-for="stat in $store.state.selectedPokemon.stats"
+          v-bind:key="stat.stat.url"
+          class="list-group-item "
+        >
+         {{ stat.base_stat }}
+        <span class="badge">{{ stat.stat.name }}</span>
+        </li>
+      </ul>
     </div>
-    <h3>Moves</h3>
+
+    <h3 id="move-label">Moves</h3>
     <ul id="move-list" class="list-group">
       <PokemonInfoMoveListItem
         v-for="move in $store.state.selectedPokemon.moves"
-        v-bind:key= "move.move.url"
+        v-bind:key="move.move.url"
         v-bind:move="move"
       />
     </ul>
@@ -61,10 +64,12 @@
 <script lang="ts">
 import { Component, Prop, Vue } from 'vue-property-decorator'
 import PokemonInfoMoveListItem from '../components/PokemonInfoMoveListItem.vue'
+import PokemonName from '../components/PokemonName.vue'
 
 @Component({
   components: {
-    PokemonInfoMoveListItem
+    PokemonInfoMoveListItem,
+    PokemonName
   }
 })
 export default class PokemonInfo extends Vue {}
@@ -74,16 +79,34 @@ export default class PokemonInfo extends Vue {}
 h3 {
   margin: 40px 0 0;
 }
+
+#move-label {
+  text-align: center;
+  margin: 40px 0 0;
+}
 ul {
   list-style-type: none;
   padding: 0;
 }
+
 li {
   display: inline-block;
   margin: 0 10px;
 }
 
 #move-list {
-    margin: auto;
+  margin: auto;
+}
+
+#pokemon-name {
+  text-align: center;
+}
+
+#pokemon-image {
+  display: block;
+  margin-left: auto;
+  margin-right: auto;
+  width: 180px;
+  height: auto;
 }
 </style>
